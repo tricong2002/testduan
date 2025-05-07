ffe76dd0294dd34e05936e35f85e8e4ac12a7b62
userdemo <truongnvps38421@gmail.com>
2025-05-07 15:18:53 +0700

    getall

--- app/Http/Controllers/Questions.php
+++ app/Http/Controllers/Questions.php
@@ -10,6 +10,7 @@ class Questions extends Controller
     public function index(Request $request)
     {
         $data = Post::all(); // Lấy tất cả các bài viết
+        dd($data);
         return response()->json($data); // Trả về dữ liệu dưới dạng JSON
     }
 }
